d9ce4ece2be0486abd1d6b2993174859ca4cc644
Leon Coto <leoncoto@gmail.com>
2024-10-10 08:39:13 +0100

    extract swap speed and rename dimensions.ts to settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -7,3 +7,5 @@ export const contentWidth = cellSize * gridCount + gap * (gridCount - 1);
 
 export const gameWidth = contentWidth + padding * 2;
 export const gameHeight = gameWidth + padding + cellSize;
+
+export const swapSpeed = { slow: 0.15, fast: 0.03 };
