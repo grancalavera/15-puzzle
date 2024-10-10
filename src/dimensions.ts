87cf3a122354b8a653e2c4f068a3a93817208640
Leon Coto <leoncoto@gmail.com>
2024-10-10 08:32:52 +0100

    make tiles smaller to get a better fit on mobile devices

--- src/dimensions.ts
+++ src/dimensions.ts
@@ -2,7 +2,7 @@ import { gridCount } from "./model";
 
 export const gap = 2;
 export const padding = 8;
-export const cellSize = 100;
+export const cellSize = 90;
 export const contentWidth = cellSize * gridCount + gap * (gridCount - 1);
 
 export const gameWidth = contentWidth + padding * 2;
