e330ac115131679081b532306fe1454a949b0b6e
Leon Coto <leoncoto@gmail.com>
2024-10-20 15:10:32 +0100

    improve shuffling

--- src/settings.ts
+++ src/settings.ts
@@ -9,7 +9,7 @@ export const buttonWidth = contentWidth / 2 - gap / 2;
 export const gameWidth = contentWidth + padding * 2;
 export const gameHeight = gameWidth + padding + cellSize;
 export const swapSpeed = { slow: 0.15, fast: 0.07 };
-export const shuffleCount = 50;
+export const shuffleCount = 100;
 export const gridSize = cellSize * gridCount + gap * 3;
 export const borderRadius = 6;
 
